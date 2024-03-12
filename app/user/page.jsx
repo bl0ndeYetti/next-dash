eda220f9d32d501743036a137331e1acb98789f0
bl0ndeYetti <mstansky08@gmail.com>
2024-03-12 09:43:20 -0400

    add page

--- app/user/page.jsx
+++ app/user/page.jsx
@@ -1,4 +1,4 @@
-export default function LoginPage() {
+export default function Page() {
   return (
     <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
       MAIN PAGE
