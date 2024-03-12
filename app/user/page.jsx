f8d70567dd444a765e926c4da8f2122c0db339d4
bl0ndeYetti <mstansky08@gmail.com>
2024-03-12 09:37:30 -0400

    daily commit

---
+++ app/user/page.jsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+      MAIN PAGE
+    </div>
+  );
+}
